4a121fd7f31658d8c32134e3b25c0cc9a69b01fe
Sharipin <murisich@gmail.com>
2013-08-12 23:03:32 +0300

    +

--- app/models/micropost.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Micropost < ActiveRecord::Base
-  belongs_to :user
-
-  validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
-
-  default_scope -> { order('created_at DESC') }
-end
